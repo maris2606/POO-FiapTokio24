0bcb4184073b244e6dbdf1078e64d4e93993f31a
labsfiap <labsfiap@PA603MICRO18.fiap.com.br>
2024-10-22 15:20:47 -0300

    COMMIT INICIAL

--- POOFiapTokio24/src/aulas/HelloWorld.java
+++ POOFiapTokio24/src/aulas/HelloWorld.java
@@ -1,8 +1,10 @@
 package aulas;
 
-public class aula1 {
+public class HelloWorld {
+
 	public static void main(String[] args) {
-		System.out.println("hello world");
+
+		System.out.println("Hello world!");
 		
 	}
 
--- POOFiapTokio24/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module POOFiapTokio24 {
-}
