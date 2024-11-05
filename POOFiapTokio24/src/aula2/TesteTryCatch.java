fae0ff21148c025e1fb3fb3f60026db2163f8c58
labsfiap <labsfiap@PA603MICRO18.fiap.com.br>
2024-11-05 16:47:07 -0300

    adicionado trycatch

---
+++ POOFiapTokio24/src/aula2/TesteTryCatch.java
@@ -0,0 +1,27 @@
+package aula2;
+
+public class TesteTryCatch {
+
+	public static void main(String[] args) {
+		int v1=1, v2=0, res=0;
+		
+		char lista[] = {1,2,3};
+		
+		for (int i =0; i<lista.length; i++) {
+			try {
+				if(i==0)
+					res = v1/v2;
+				else
+					lista[3]=4;
+				
+			} catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+				System.out.println("exceção: "+ e);
+			}
+			
+			System.out.println("i = " + i);
+			System.out.println("res = " + res);
+		}
+
+	}
+
+}
