c8a32830a1bc135a9141a8158702c6c7dedcf686
maris <marisa.mayumi.mo@gmail.com>
2024-11-07 16:44:45 -0300

    add outros testes

--- POOFiapTokio24/src/aula7/ex2/ex3.java
+++ POOFiapTokio24/src/aula7/ex2/ex3.java
@@ -12,24 +12,24 @@ public class ex3 {
 
 	public static void main(String[] args) {
 
-//		Collection<Integer> lista = new ArrayList<Integer>(); 
+//		Collection<Integer> lista = new ArrayList<Integer>(); // melhor com muitos registros
 		// 50.000 4 ms
 		// 100.000 5 ms     !
 		// 1.000.000 22 ms  !
 		
-//		Collection<Integer> lista = new HashSet<Integer>(); 
+		Collection<Integer> lista = new HashSet<Integer>(); // 2° melhor com 10 000 000
 		// 50.000 6 ms
 		// 100.000 11 ms
 		// 1.000.000 72 ms
 		
-		Collection<Integer> lista = new LinkedList<Integer>(); 
+//		Collection<Integer> lista = new LinkedList<Integer>(); // melhor com - registros
 		// 50.000 3 ms      !
 		// 100.000 6 ms
 		// 1.000.000 102 ms
 		
 		long inicio = System.currentTimeMillis();
 		
-		for (int i = 0; i<1000000; i++) {
+		for (int i = 0; i<10_000_000; i++) {
 			lista.add(i);
 		}
 		
